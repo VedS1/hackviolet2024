63754936aac20b8eda4bff9a410710d6ae079ae7
VedS1 <46610410+VedS1@users.noreply.github.com>
2024-02-03 14:50:50 -0500

    a

---
+++ backend/models/course.js
@@ -0,0 +1,23 @@
+const { listenerCount } = require("events");
+const mongoose = require ("mongoose");
+const CourseSchema = new mongoose.Schema({
+  courseName: {
+      type: String,
+      required: false,
+  },
+  courseDate: {
+      type: String,
+      required: false,
+  },
+  courseImage: {
+      type: String,
+      required: false,
+  },
+});
+
+// eslint-disable-next-line no-undef
+courseDb = mongoose.createConnection("mongodb+srv://vedsoolgiri:OnlyLetters@cluster0.8nnfpb9.mongodb.net/?retryWrites=true&w=majority");
+
+// eslint-disable-next-line no-undef
+const Course = userDb.model("Course", CourseSchema);
+module.exports = Course;
