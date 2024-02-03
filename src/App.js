e62aa26e5d3559f150a50d4c9c4cb2708db0d34d
Tommy <tommy.hiphop@yahoo.com>
2024-02-03 13:46:26 -0500

    login1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+
+const onButtonClick = () => {
+
+}
 
 function App() {
     return (
         <div className="mainContainer">
             <div className="inputContainer">
                 <input
-                    placeholder="Enter your password here"
+                    placeholder="Enter your email here"
                     className={"inputBox"}
                 />
             </div>
             <div className="inputContainer">
                 <input
-                    placeholder="Enter your email here"
+                    placeholder="Enter your password here"
                     className={"inputBox"}/>
             </div>
+            <div className={"inputContainer"}>
+                <input
+                    className={"loginButton"}
+                    type={"button"}
+                    onClick={onButtonClick}
+                    value={"log in"}/>
+            </div>
         </div>
-);
+    );
 
 }
 
