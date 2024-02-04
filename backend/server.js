d9c363991739343c9914836fb672eedd48a1de40
VedS1 <46610410+VedS1@users.noreply.github.com>
2024-02-03 21:45:55 -0500

    m

--- backend/server.js
+++ backend/server.js
@@ -12,22 +12,21 @@ const { response } = require("express");
 app.use(express.json());
 app.use(cors());
 
-
-app.post("/login", async (req, res) =>{  //check if an account exists
+app.post("/login", async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    console.log(req)
-    UserModel.find({email: email, password: password}, (err, result)=>{
-        if(err){
-            res.send(err);
+
+    try {
+        const result = await UserModel.find({ email: email, password: password }).exec();
+        if (result && result.length > 0) {
+            res.send(result);
+        } else {
+            res.send("Wrong information");
         }
-        if(result){
-            res.send(result);}
-        else{
-            res.send("Wrong information")
-        }
-    })
-})
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 
 app.post ('/signup', async(req, res) =>{
